2b4576f5049cfa6ac723e654317a56ede0196de2
Cooper-Frye <cooperf@stanford.edu>
2015-11-13 18:52:18 -0800

    no message

--- Database Download Tools/DownloadManyPages.py
+++ Database Download Tools/DownloadManyPages.py
@@ -22,41 +22,42 @@ lock = threading.Lock()
 countDownLock = threading.Lock()
 
 def callDownloadSinglePage(cardNumber):
-	try:
-		DownloadSinglePage.downloadSinglePage(str(cardNumber), False)
-		with countDownLock:
-			if thisverbose: print 'Got card number ' + str(cardNumber)
-			#cardsLeft = cardsLeft - 1
-	except:
-		with lock:
-			print 'FAILURE!!!! CARD NUMBER ' + str(cardNumber)
-			f = open('FAILURES.txt','a')
-			f.write(str(cardNumber)+'\n')
-			f.close()
+    try:
+        DownloadSinglePage.downloadSinglePage(str(cardNumber), False)
+        with countDownLock:
+            if thisverbose: print 'Got card number ' + str(cardNumber)
+            #cardsLeft = cardsLeft - 1
+    except:
+        with lock:
+            print 'FAILURE!!!! CARD NUMBER ' + str(cardNumber)
+            f = open('FAILURES.txt','a')
+            f.write(str(cardNumber)+'\n')
+            f.close()
 
 def main(argv):
-	#indicate start
-	print "Beginning download of every "+str(incrementCard)+'-th card'
-	print 'in the range' +str(startCard)+'-'+str(endCard)
-	
-	#generate list of cards to spit out
-	cardList = []
-	x = startCard
-	while x<=endCard:
-		cardList.append(x)
-		x+=incrementCard
-		
-	#empty the failure file	
-	f = open('FAILURES.txt','a')
-	f.truncate()
-	f.close()
-	
-	#thread operation
-	p = Pool(numThreads)
-	p.map(callDownloadSinglePage, cardList)
-	
-	
-	print 'Done!'
-	
+    #indicate start
+    print "Beginning download of every "+str(incrementCard)+'-th card'
+    print 'in the range' +str(startCard)+'-'+str(endCard)
+    
+    #generate list of cards to spit out
+    f = open('failuresTrue.txt', 'r')
+    cardList = f.readlines()
+    #x = startCard
+    #while x<=endCard:
+    #   cardList.append(x)
+    #   x+=incrementCard
+        
+    #empty the failure file 
+    f = open('FAILURES.txt','a')
+    f.truncate()
+    f.close()
+    
+    #thread operation
+    p = Pool(numThreads)
+    p.map(callDownloadSinglePage, cardList)
+    
+    
+    print 'Done!'
+    
 if __name__ == '__main__':
-	main(sys.argv)
+    main(sys.argv)
