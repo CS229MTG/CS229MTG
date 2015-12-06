06902cb24712bf3130624963b899b8c9bc8e08e8
Cooper-Frye <cooperf@stanford.edu>
2015-12-06 01:05:43 -0800

    now does PCA

--- MilestoneSVMRegression.py
+++ MilestoneSVMRegression.py
@@ -17,7 +17,7 @@ from sklearn import svm
 from sklearn import cross_validation as cv
 from sklearn import metrics 
 from sklearn import preprocessing
-
+from sklearn.decomposition import PCA
 
 
 
@@ -33,8 +33,6 @@ def constructTrainingMatrix(startNumber,endNumber, labelGap, convertToSparse=Fal
         priceVector=DataUtils.parseIntoPriceOnlyList(x)
         # card attributes vector=Utils.parseIntoAttributeList(x,parsedData)
         if not priceVector==None:
-            if priceVector[-1]<100:
-                continue
             #print 'var numDays=' + str(numDays)
             #print 'num Days for this one is '+ str(len(priceVector))
             #if numDays == 0:
@@ -52,7 +50,7 @@ def constructTrainingMatrix(startNumber,endNumber, labelGap, convertToSparse=Fal
     #listOfpriceVectors=[array(trainingVec) for trainingVec in listOfpriceVectors] #converts each vector to array form for next step; might change order, shouldn't matter
     #trainingMatrix=np.zeroes((numVectors,numDays), np.int16)
     trainingMatrix=np.matrix(listOfTrainingVectors, dtype=float)
-    print 'using card numbers' + str(cardsUsed)
+    #print 'using card numbers' + str(cardsUsed)
     #print 'trainingMatrix is ' 
     #print trainingMatrix
     #print 'y Vector is '
@@ -61,13 +59,7 @@ def constructTrainingMatrix(startNumber,endNumber, labelGap, convertToSparse=Fal
     
 def trainEstimator(X, y,):
     (X_train,X_test,y_train,y_test)=cv.train_test_split(X,y,test_size=0.4, random_state=0)
-    #if verbose:
-    #    print 'size of whole example matrix \t' + str(X.shape) 
-    #    print 'size of whole output vector \t' + str((np.array(y)).shape)
-    #    print 'size of training example matrix ' + str(X_train.shape)
-    #    print 'size of training output vector \t' + str((np.array(y_train)).shape)
-    #    print 'size of test example matrix \t' + str(X_test.shape)
-    #    print 'size of test output vector \t' + str((np.array(y_test)).shape)
+    
     #svr= svm.SVR(kernel='rbf', C=1e3, gamma=0.1)
     svr = svm.SVR(kernel='linear', C=10)
     #svr = svm.SVR(kernel='poly', C=1e3, degree=2)
@@ -108,38 +100,28 @@ def main(argv):
     (X,y)=constructTrainingMatrix(startNumber, endNumber, labelGap)
     
     scaler=preprocessing.StandardScaler().fit(X)
-    X_scaled=scaler.transform(X)
-    
-    #X_scaled=preprocessing.scale(X)
-    if verbose:
-        print scaler
-        print scaler.mean_
-        print 'the mean of the scaled data is ' + str(X_scaled.mean(axis=0))
-        print 'the std of the scaled data is ' + str(X_scaled.std(axis=0))
-        print 'here is the data'+ str(X)
-        print 'here is the scaled data'+ str(X_scaled)
+    X=scaler.transform(X)
+    #print 'scaled x:' + str(X)
+    pca=PCA(n_components=10)
+    pca.fit(X)
+    #print str(pca)
+    print pca.explained_variance_ratio_
+    X=pca.transform(X)
+    #print 'transformed X:' + str(X)
     #X is a matrix with n_examples rows and n_days columns, y is a n_examples long vector of what the prices were labelGap days in the future
     
     #score=trainEstimator(X,y)
     #print 'labelGap:score without scaling \n' + str(labelGap)+ ':'+str(score)
-    (X_train,X_test,y_train,y_test)=cv.train_test_split(X_scaled,y,test_size=0.4, random_state=0)
-    #if verbose:
-    #    print 'size of whole example matrix \t' + str(X.shape) 
-    #    print 'size of whole output vector \t' + str((np.array(y)).shape)
-    #    print 'size of training example matrix ' + str(X_train.shape)
-    #    print 'size of training output vector \t' + str((np.array(y_train)).shape)
-    #    print 'size of test example matrix \t' + str(X_test.shape)
-    #    print 'size of test output vector \t' + str((np.array(y_test)).shape)
-    #svr= svm.SVR(kernel='rbf', C=1e3, gamma=0.1)
+    (X_train,X_test,y_train,y_test)=cv.train_test_split(X,y,test_size=0.4, random_state=0)
+    #svr= svm.SVR(kernel='rbf', C=10, gamma=0.1)
     svr = svm.SVR(kernel='linear', C=10)
-    #svr = svm.SVR(kernel='poly', C=1e3, degree=2)
+    #svr = svm.SVR(kernel='poly', C=1, degree=2)
     svr= svr.fit(X_train, y_train)
     y_pred = svr.predict(X_test)
     #print 'y values for the test set ' + str(y_test)
     #print 'predicted y for test set using svr is ' + str(y_pred) 
     score=svr.score(X_test,y_test)
-    
-    #svr_poly= svr_poly.fit(X, y)
+    #print 'weights' + str(svr.coef_)
     print 'labelGap:score with scaling \n' + str(labelGap)+ ':'+str(score)
     
     
